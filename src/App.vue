68561063c912426cffdec13c8efbf48b856a1579
Jason <chenzjchina@gmail.com>
2016-10-13 16:41:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1 v-on:click="click" v-if="seen">{{ msg }}</h1>
+    <Hi v-bind:call="callBack" foo="foo222"></Hi>
+  </div>
+</template>
+
+<script>
+import Hi from './Hi.vue'
+
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue!',
+      seen: true
+    }
+  },
+  methods: {
+    click: function (e) {
+      e.preventDefault();
+      alert(this.msg)
+    },
+    callBack: function(data) {
+      alert(`from sub data call foo ${data}`);
+    }
+  },
+  components: {
+    'Hi': Hi
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/Hi.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="hi">
+    <h1 v-on:click="click">{{ msg }}{{ foo }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    foo: {
+      type: String
+    },
+    call: Function
+  },
+  data () {
+    return {
+      msg: 'i am hi component'
+    }
+  },
+  methods: {
+    click: function(e) {
+      e.preventDefault();
+      this.call('hihihi')
+    }
+  }
+}
+</script>
+
+<style>
+#hi {
+  background-color: red
+}
+</style>
